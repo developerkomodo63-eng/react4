0e781cce10263c5718937dae2a135dbdeece188e
unknown <sistemas@VRE_LAB21022.inacap.cl>
2026-06-22 19:56:36 -0400

    xddddddde

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </div>
         <div className="intro">
           <h1>Hola, alexander jerez</h1>
-          <p>Este dashboard funciona a base de café, memes y magia de Git — 99% caos, 1% estabilidad. Weno sipo, mi sangre.</p>
+          <p>un saluo pal guaton bairon que me debe 2 luca, puro enfasi.</p>
         </div>
         <button
           type="button"
